7e455a6726692cba625e08b6fc87debc624ea46c
duck3866 <130462482+duck3866@users.noreply.github.com>
2024-09-25 14:38:15 +0900

    Feat :: monsterScene 추가,MonsterInfo 추가

---
+++ Assets/02.Scripts/Monster/GeneratorMonsterInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorMonsterInfo
+{
+   public GameObject movePositionGroup;
+   public MonsterManager.MonsterType monsterType;
+   public bool isMoving = true;
+   public Transform startPosition;
+}
--- Assets/02.Scripts/Monster/MonsterManager.cs
+++ Assets/02.Scripts/Monster/MonsterManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MonsterManager : Singleton<MonsterManager>, IManager
 {
+    public List<GeneratorMonsterInfo> generatorMonsterInfos;
+    public enum MonsterType
+    {
+        Student = 0,
+        Mirror = 1
+    }
     public void Initialize(string sceneName)
     {
         if(sceneName == SceneConstants.PlaygroundA) // 현재 씬이 PlaygroundA라면
