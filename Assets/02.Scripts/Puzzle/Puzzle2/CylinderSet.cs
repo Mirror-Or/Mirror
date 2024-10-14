eb45bb5b1e4e8e27f2cd1d78eb0e294bef3651e7
harp515 <harp5151017@gmail.com>
2024-10-14 14:11:12 +0900

    refactor :: CylinderSet에서만 SpinCylinder를 참조하도록 변경

--- Assets/02.Scripts/Puzzle/Puzzle2/CylinderSet.cs
+++ Assets/02.Scripts/Puzzle/Puzzle2/CylinderSet.cs
@@ -1,46 +1,36 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class CylinderSet : RaycastCheck, IInteractionable
 {
-    [Tooltip("실린더 순서대로 넣기")]
-    [SerializeField] private SpinCylinder[] spinCylinder;     // 회전할 객체를 넣을 리스트
-    
     [Tooltip("최대 회전 횟수")]
-    public int[] cylinderSpinSet;           // 각 spinCylinder마다 회전할 수 있는 최대 회전 수
-    
+    [SerializeField] private bool isYSpin;                // Y축 회전을 사용할 것인가
+    [Tooltip("실린더 순서대로 넣기")]
+    [SerializeField] private List<SpinCylinder> spinCylinder;     // 회전할 객체를 넣을 리스트
     [Tooltip("각 실린더 별 답")]
-    [SerializeField] private int[] puzzleAnswer;              // 각 실린더 별로 맞춰져야하는 답
+    [SerializeField] private List<int> puzzleAnswer;              // 각 실린더 별로 맞춰져야하는 답
     
-    [Tooltip("각 실린더 별 현재 답")]
-    public int[] puzzleNowAnswer;           // 각 실린더의 현재 답
-
+    [SerializeField] private float speed;                     // 실린더들의 회전 속도
     [SerializeField] private bool test;     // 상호작용 테스트 용 변수   *임시*
-    
-    public float speed;   // 실린더들의 회전 속도
+    [SerializeField] private int[] CylinderSpinSet;           // 각 spinCylinder마다 회전할 수 있는 최대 회전 수
     [SerializeField] private Camera myCam;  // Raycast에 사용되는 카메라
 
-    private bool interaction;
     
-    private bool isOpen;                      // 답이 맞을 경우 true가 
+    private bool _interaction;
+    private List<int> _puzzleNowAnswer;           // 각 실린더의 현재 답
+    private bool _isOpen;                      // 답이 맞을 경우 true가
     
     void Awake()
     {
-        // puzzleNowAnswer의 리스트 수를 초기화
-        puzzleNowAnswer = new int[puzzleAnswer.Length];
-        
-        // 각 실린더별 초기 세팅을 위한 반복문
-        for (int i = 0; i < spinCylinder.Length; i++)
+        for (int i = 0; i < spinCylinder.Count; i++)
         {
-            // 컴포넌트 받아오기
-            spinCylinder[i] = spinCylinder[i].GetComponent<SpinCylinder>();
-            // SpinCylinder의 순서를 i로 지정
-            spinCylinder[i].thisCylinderNum = i;
-            // puzzleNowAnswer의 현재 답을 0으로 세팅
-            puzzleNowAnswer[i] = 0;
+            spinCylinder[i].Init(speed, CylinderSpinSet[i], isYSpin);
         }
+        
+        // puzzleNowAnswer의 리스트 수를 초기화
+        _puzzleNowAnswer = Enumerable.Repeat(0, spinCylinder.Count).ToList();
     }
-
     void Update()
     {
         if (test)       // 상호작용 테스트 용  *임시*
@@ -54,38 +44,32 @@ public class CylinderSet : RaycastCheck, IInteractionable
             // 카메라를 끈다
             myCam.gameObject.SetActive(false);
             // 상호작용 종료
-            interaction = false;
+            _interaction = false;
         }
         
-        if (!interaction) return;   // 상호 작용 중일때만 사용할 수 있도록 함
+        if (!_interaction) return;   // 상호 작용 중일때만 사용할 수 있도록 함
         
         // puzzleNowAnswer와 puzzleAnswer의 리스트 값이 같을 경우
-        if (puzzleAnswer.SequenceEqual(puzzleNowAnswer))
+        if (puzzleAnswer.SequenceEqual(_puzzleNowAnswer))
         {
             // 클리어 판정
-            isOpen = true;
+            _isOpen = true;
             Debug.Log("Clear!");
+            // 카메라를 끈다
+            myCam.gameObject.SetActive(false);
+            _interaction = false;
         }
         // 좌클릭을 했을 때
         if (Input.GetMouseButtonDown(0))
         {
-            CylinderClick();
-        }
-    }
-
-    private void CylinderClick()
-    {
-        // 클릭한 객체가 SpinCylinder일 때 PuzzleClick을 실행함
-        RayHitCheck(Input.mousePosition, myCam).transform.GetComponent<SpinCylinder>()?.PuzzleClick();
-    }
-    // 퍼즐의 현재 답을 바꿈
-    public void ChangeNowAnswer(int cylinderNum)
-    {
-        puzzleNowAnswer[cylinderNum]++;
-        // 만약 기본 값이 현재값보다 같거나 작을 시 0으로 바꿈
-        if (cylinderSpinSet[cylinderNum] <= puzzleNowAnswer[cylinderNum])
-        {
-            puzzleNowAnswer[cylinderNum] = 0;
+            // 클릭한 객체가 SpinCylinder일 때 PuzzleClick을 실행함
+            var cylinderObj = RayHitCheck(Input.mousePosition, myCam).transform;
+            var findIndex = spinCylinder.FindIndex(n => n.transform == cylinderObj);
+            if (findIndex != -1)
+            {
+               _puzzleNowAnswer[findIndex] = cylinderObj.GetComponent<SpinCylinder>().PuzzleClick();
+            }
+            
         }
     }
     // 플레이어가 상호작용을 진행했을 경우
@@ -94,6 +78,6 @@ public class CylinderSet : RaycastCheck, IInteractionable
         // 카메라를 켠다
         myCam.gameObject.SetActive(true);
         // 퍼즐을 풀 수 있도록 한다
-        interaction = true;
+        _interaction = true;
     }
 }
--- Assets/02.Scripts/Puzzle/Puzzle2/SpinCylinder.cs
+++ Assets/02.Scripts/Puzzle/Puzzle2/SpinCylinder.cs
@@ -1,77 +1,86 @@
-using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class SpinCylinder : MonoBehaviour
 {
-    private CylinderSet cylinderSet;   // CylinderSet에서 지정된 값 받아오는 용도
-    [SerializeField] private bool isYSpin;                // Y축 회전을 사용할 것인가
-
-    [HideInInspector] public int thisCylinderNum; // CylinderSet이 지정한 번호를 받음
-
-    private float spinRotate;   // 한 번에 회전하는 각도
-    private bool isSpin;          // 현재 회전 중인지 확인하는 용도
-    private float speed;        // CylinderSet이 지정한 회전 속도를 받아오는 용도
-    private float waitTime;     // 회전을 할 때 기다리는 시간
+    private bool _isSpinAngle;                // Y축 회전을 사용할 것인가
+    private float _spinRotate;   // 한 번에 회전하는 각도
+    private bool _isSpin;          // 현재 회전 중인지 확인하는 용도
+    private float _speed;        // CylinderSet이 지정한 회전 속도를 받아오는 용도
+    private float _waitTime;     // 회전을 할 때 기다리는 시간
     
-    private void Awake()
+    private int _cylinderNum;
+    private int _nowAnswer;
+    private float _cylinderSpinSet;
+
+    public void Init(float spinSpeed, float spinSet, bool spinAngle)
     {
-        cylinderSet = gameObject.transform.parent.GetComponent<CylinderSet>();
         // 초기 값 세팅
-        waitTime = 1f / cylinderSet.speed;
-        speed = cylinderSet.speed;
-        
+        _speed = spinSpeed;
+        _cylinderSpinSet = spinSet;
+        _isSpinAngle = spinAngle;
+    }
+
+    private void Start()
+    {
+        _waitTime = 1f / _speed;
         // CylinderSet이 설정한 면 수에 맞춰서 회전 각도를 지정함
-        spinRotate = 360f / ~cylinderSet.cylinderSpinSet[thisCylinderNum];
+        _spinRotate = 360f / _cylinderSpinSet;
     }
 
     // CylinderSet에서 실행시킴
-    public void PuzzleClick()
+    public int PuzzleClick()
     {
         // 회전 중이 아닐 때 (연속 회전 방지)
-        if (isSpin) return;
+        if (_isSpin) return _nowAnswer;
         
-        isSpin = true;
-        // CylinderSet에서 가지고 있는 이 객체의 현재 회전 값을 올림
-        cylinderSet.ChangeNowAnswer(thisCylinderNum);
-        
+        _isSpin = true;
         // 회전 코루틴 실행
         StartCoroutine(Spin());
+            
+        // CylinderSet에서 가지고 있는 이 객체의 현재 회전 값을 올림
+        _nowAnswer++;
+        // 만약 기본 값이 현재값보다 같거나 작을 시 0으로 바꿈
+        if (_cylinderSpinSet <= _nowAnswer)
+        {
+            _nowAnswer = 0;
+        }
+        return _nowAnswer;
     }
 
     private void Update()
     {
-        if(!isSpin) return;
+        if(!_isSpin) return;
         
-        if (!isYSpin) // y축 회전이 아닐 때
+        if (!_isSpinAngle) // y축 회전이 아닐 때
         {
             // 정해진 각도만큼 회전
-            transform.Rotate(spinRotate * Time.deltaTime * speed, 0, 0);
+            transform.Rotate(_spinRotate * Time.deltaTime * _speed, 0, 0);
         }
         else // y축 회전일 때
         {
             // 정해진 각도만큼 회전
-            transform.Rotate(0, spinRotate * Time.deltaTime * speed, 0);
+            transform.Rotate(0, _spinRotate * Time.deltaTime * _speed, 0);
         }
     }
 
     private IEnumerator Spin()
     {
         // waitTime 이상의 시간이 지났을 경우 다음 코드를 진행
-        yield return new WaitForSeconds(waitTime);
+        yield return new WaitForSeconds(_waitTime);
 
-        if (!isYSpin)     // y축 회전이 아닐 때
+        if (!_isSpinAngle)     // y축 회전이 아닐 때
         {
             // 어긋난 각도 재조정
-            transform.localRotation = Quaternion.Euler(spinRotate * cylinderSet.puzzleNowAnswer[thisCylinderNum], 0, 0);
+            transform.localRotation = Quaternion.Euler(_spinRotate * _nowAnswer, 0, 0);
         }
         else        // y축 회전일 때
         {
             // 어긋난 각도 재조정
-            transform.localRotation = Quaternion.Euler(0, spinRotate * cylinderSet.puzzleNowAnswer[thisCylinderNum], 0);
+            transform.localRotation = Quaternion.Euler(0, _spinRotate * _nowAnswer, 0);
         }
+        
         // spin 초기화
-        isSpin = false;
+        _isSpin = false;
     }
 }
