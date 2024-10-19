f31a9a16ca4a3d60da29294c1150a1af2cc3132f
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2024-10-20 01:33:03 +0900

    fix: 공격이 두번씩 나가는 문제 해결
    너무 빨리 Idle 상태로 전환되는 게 문제

--- Assets/02.Scripts/Player/State/AttackState.cs
+++ Assets/02.Scripts/Player/State/AttackState.cs
@@ -5,9 +5,13 @@ public class AttackState : IPlayerState
 {
     private PlayerCombatController _combatController;
     private Vector3 _playerPosition;
-    private LayerMask _enemyLayer;
+    private int _enemyLayer;
 
-    public AttackState(PlayerCombatController combatController, Vector3 playerPosition, LayerMask enemyLayer)
+    // 공격 시간 관련 변수
+    private float _attackDuration = 1.0f;  // 공격이 끝날 때까지 기다릴 시간
+    private float _attackTimer;
+
+    public AttackState(PlayerCombatController combatController, Vector3 playerPosition, int enemyLayer)
     {
         _combatController = combatController;
         _playerPosition = playerPosition;
@@ -16,17 +20,23 @@ public class AttackState : IPlayerState
 
     public void EnterState(PlayerFSM playerFSM)
     {
-        Debug.Log("Entered Attack State");
+        Debug.Log("Attack 상태 시작");
         _combatController.PerformAttack(_playerPosition, _enemyLayer);
+         _attackTimer = _attackDuration;  // 상태가 시작되면 타이머 초기화
     }
 
     public void UpdateState(PlayerFSM playerFSM)
     {
-        playerFSM.ChangeState(new IdleState()); // 공격 후 Idle 상태로 전환
+        _attackTimer -= Time.deltaTime;  // 공격 타이머 감소
+
+        if(_attackTimer <= 0.0f)
+        {
+            playerFSM.ChangeState<IdleState>();         // 공격 후 Idle 상태로 전환
+        }
     }
 
     public void ExitState(PlayerFSM playerFSM)
     {
-        Debug.Log("Exiting Attack State");
+        Debug.Log("Attack 상태 종료");
     }
 }
