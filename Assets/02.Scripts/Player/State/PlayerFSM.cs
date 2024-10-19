d9a229f2aac629b97df11840e09e203903956045
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2024-10-20 01:34:20 +0900

    update : FSM에서 상태 변경 기능 업데이트

--- Assets/02.Scripts/Player/State/PlayerFSM.cs
+++ Assets/02.Scripts/Player/State/PlayerFSM.cs
@@ -8,25 +8,34 @@ using Utils;
 public class PlayerFSM
 {
     public IPlayerState currentState;
+    private Dictionary<Type, IPlayerState> _stateCache = new();
+
     public PlayerMovementController movementController;
     public PlayerCombatController combatController;
-    public PlayerCameraController cameraController;
     public PlayerInputAction inputActions;
 
-    public PlayerFSM(PlayerMovementController movementController, PlayerCombatController combatController, PlayerInputAction inputActions, PlayerCameraController cameraController)
+    public PlayerFSM(PlayerMovementController movementController, PlayerCombatController combatController, PlayerInputAction inputActions)
     {
         this.movementController = movementController;
         this.combatController = combatController;
         this.inputActions = inputActions;
-        this.cameraController = cameraController;
     }
 
-    public void ChangeState(IPlayerState newState)
+    // 상태 변경 시 State 캐싱
+    public void ChangeState<T>(params object[] args) where T : IPlayerState
     {
-        if (currentState != null)
-            currentState.ExitState(this);
+        // 현재 상태에서 Exit
+        currentState?.ExitState(this);
 
-        currentState = newState;
+        if(_stateCache.ContainsKey(typeof(T)))
+        {
+            currentState = _stateCache[typeof(T)];
+        }else{
+            Debug.Log($"{args.Length} : {args}");
+            currentState = (T)Activator.CreateInstance(typeof(T), args);
+            _stateCache[typeof(T)] = currentState;
+        }
+
         currentState.EnterState(this);
     }
 
